9a07917b561fbe91ca96e14d2f610cb924bc9a93
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-07 10:30:08 -0700

    Adds feature and model test for average age

--- spec/features/user_sees_astronauts_spec.rb
+++ spec/features/user_sees_astronauts_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-# User Story 1 of 4
+# User Story 2 of 4
 # 
 # As a visitor,
 # When I visit '/astronauts'
-# I see a list of astronauts with the following info:
-# - Name
-# - Age
-# - Job
+# I see the average age of all astronauts.
 # 
-# (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander")
+# (e.g. "Average Age: 34")
 
 describe 'As a visitor to the app' do
   it 'I see a list of astronauts' do
@@ -24,5 +21,14 @@ describe 'As a visitor to the app' do
     expect(page).to have_content("Job: #{astronaut_2.job}")
     expect(page).to have_content("Age: #{astronaut.age}")
     expect(page).to have_content("Age: #{astronaut_2.age}")
+  end
+  it 'sees average age of astronauts' do
+    astronaut = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+    astronaut_2 = Astronaut.create(name: "Buzz Aldrin", age: 39, job: "Commander")
+    astronaut_3 = Astronaut.create(name: "Astronaut", age: 39, job: "Commander")
+    
+    visit astronauts_path
+    
+    expect(page).to have_content("Average Age: #{Astronaut.average_age}")
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,17 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many(:missions).through(:astronaut_missions)}
   end
+  
+  describe 'Class Methods' do
+    describe '.average_age' do
+      it 'should return the average age' do
+        Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+        Astronaut.create(name: "Buzz Aldrin", age: 39, job: "Commander")
+        Astronaut.create(name: "Astronaut", age: 40, job: "Commander")
+        average_age = 38.67
+        
+        expect(Astronaut.average_age).to eq(average_age)
+      end
+    end
+  end
 end
