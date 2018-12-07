81b010d811fc7a70636f2da1ed089fb2737ea893
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-07 10:57:19 -0700

    Cleans up tests

--- spec/features/user_sees_astronauts_spec.rb
+++ spec/features/user_sees_astronauts_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
-# User Story 4 of 4
-# 
-# As a visitor,
-# When I visit '/astronauts'
-# I see the total time in space for each astronaut.
-# (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander, Total Time in Space: 760 days")
-
 describe 'As a visitor to the app' do
   it 'I see a list of astronauts' do
     astronaut = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
@@ -14,12 +7,16 @@ describe 'As a visitor to the app' do
     
     visit astronauts_path
     
-    expect(page).to have_content(astronaut.name)
-    expect(page).to have_content(astronaut_2.name)
-    expect(page).to have_content("Job: #{astronaut.job}")
-    expect(page).to have_content("Job: #{astronaut_2.job}")
-    expect(page).to have_content("Age: #{astronaut.age}")
-    expect(page).to have_content("Age: #{astronaut_2.age}")
+    within "#astronaut-#{astronaut.id}" do
+      expect(page).to have_content(astronaut.name)
+      expect(page).to have_content("Job: #{astronaut.job}")
+      expect(page).to have_content("Age: #{astronaut.age}")
+    end
+    within "#astronaut-#{astronaut_2.id}" do
+      expect(page).to have_content(astronaut_2.name)
+      expect(page).to have_content("Job: #{astronaut_2.job}")
+      expect(page).to have_content("Age: #{astronaut_2.age}")
+    end
   end
   it 'sees average age of astronauts' do
     astronaut = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
@@ -61,10 +58,10 @@ describe 'As a visitor to the app' do
     visit astronauts_path
     
     within "#astronaut-#{astronaut.id}" do
-      expect(page).to have_content("Total Time in Space: #{astronaut.total_time_in_space}")
+      expect(page).to have_content("Total Time in Space: #{astronaut.total_time_in_space} days")
     end
     within "#astronaut-#{astronaut_2.id}" do
-      expect(page).to have_content("Total Time in Space: #{astronaut_2.total_time_in_space}")
+      expect(page).to have_content("Total Time in Space: #{astronaut_2.total_time_in_space} days")
     end
   end
   
